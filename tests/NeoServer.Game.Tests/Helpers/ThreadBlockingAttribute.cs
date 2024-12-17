701164ff38f7e6f09db7abbf415024fd0b0a3379
caioavidal <caio.avidal@gmail.com>
2024-12-17 15:22:23 -0300

    Remove comments

--- tests/NeoServer.Game.Tests/Helpers/ThreadBlockingAttribute.cs
+++ tests/NeoServer.Game.Tests/Helpers/ThreadBlockingAttribute.cs
@@ -6,44 +6,29 @@ using Xunit.Sdk;
 namespace NeoServer.Game.Tests.Helpers;
 
 /// <summary>
-///     Block the unit test to avoid concurrency
+/// Block the unit test to avoid concurrency
 /// </summary>
 public class ThreadBlockingAttribute : BeforeAfterTestAttribute
 {
     public override void Before(MethodInfo methodUnderTest) => ThreadBlocking.Wait();
-
     public override void After(MethodInfo methodUnderTest) => ThreadBlocking.Release();
 }
 
 public static class ThreadBlocking
 {
-    private static readonly Mutex Mutex = new();
-    private static readonly SemaphoreSlim SemaphoreSlim = new SemaphoreSlim(1, 1);
+    private static readonly SemaphoreSlim SemaphoreSlim = new(1, 1);
 
     public static void Wait()
     {
         try
         {
             SemaphoreSlim.Wait();
-            //Mutex.WaitOne();
-            // Mutex acquired
         }
-        catch (AbandonedMutexException)
+        catch (Exception ex)
         {
-            Console.WriteLine("Warning: Mutex was abandoned by another thread.");
+            Console.Write(ex);
         }
     }
 
-    public static void Release()
-    {
-        SemaphoreSlim.Release();
-        // if (Mutex.WaitOne(0)) // Check if mutex is owned before releasing
-        // {
-        //     Mutex.ReleaseMutex();
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Warning: Mutex was not owned by the current thread.");
-        // }
-    }
+    public static void Release() => SemaphoreSlim.Release();
 }
