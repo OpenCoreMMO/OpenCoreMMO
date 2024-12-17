c575449bdbd2033a76fe17366cfd16b95725fcc0
MarcusViniciusS <lowpoc.developer@gmail.com>
2024-12-17 17:26:47 -0300

    feat: add system text json on spell loader.

---
+++ src/Loaders/NeoServer.Loaders/Extensions/JsonTextExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+namespace NeoServer.Loaders.Extensions;
+
+public static class JsonTextExtensions
+{
+    public static uint GetUInt32FromJson(this JsonElement element)
+    {
+        return element.ValueKind switch
+        {
+            JsonValueKind.String => uint.TryParse(element.GetString(), out var value) ? value : 0,
+            JsonValueKind.Number => element.GetUInt32(),
+            _ => 0
+        };
+    }
+
+    public static ushort GetUInt16FromJson(this JsonElement element)
+    {
+        return element.ValueKind switch
+        {
+            JsonValueKind.String => ushort.TryParse(element.GetString(), out var value) ? value : (ushort)0,
+            JsonValueKind.Number => element.GetUInt16(),
+            _ => (ushort)0
+        };
+    }
+    
+    public static string GetStringFromJson(this JsonElement element)
+    {
+        return element.ValueKind switch
+        {
+            JsonValueKind.String => element.GetString(),
+            _ => null
+        };
+    }
+}
