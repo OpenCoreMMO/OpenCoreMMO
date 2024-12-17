5ba9540cbd41a8855a778d9792f28c01c89def97
caioavidal <caio.avidal@gmail.com>
2024-12-17 15:16:09 -0300

    Change Mutex by SemaphoreSlim

--- tests/NeoServer.Game.Tests/Helpers/ThreadBlockingAttribute.cs
+++ tests/NeoServer.Game.Tests/Helpers/ThreadBlockingAttribute.cs
@@ -3,48 +3,47 @@ using System.Reflection;
 using System.Threading;
 using Xunit.Sdk;
 
-namespace NeoServer.Game.Tests.Helpers
-{
-    /// <summary>
-    /// Block the unit test to avoid concurrency.
-    /// </summary>
-    public class ThreadBlockingAttribute : BeforeAfterTestAttribute
-    {
-        public override void Before(MethodInfo methodUnderTest) => ThreadBlocking.Wait();
+namespace NeoServer.Game.Tests.Helpers;
 
-        public override void After(MethodInfo methodUnderTest) => ThreadBlocking.Release();
+/// <summary>
+///     Block the unit test to avoid concurrency
+/// </summary>
+public class ThreadBlockingAttribute : BeforeAfterTestAttribute
+{
+    public override void Before(MethodInfo methodUnderTest) => ThreadBlocking.Wait();
+
+    public override void After(MethodInfo methodUnderTest) => ThreadBlocking.Release();
+}
+
+public static class ThreadBlocking
+{
+    private static readonly Mutex Mutex = new();
+    private static readonly SemaphoreSlim SemaphoreSlim = new SemaphoreSlim(1, 1);
+
+    public static void Wait()
+    {
+        try
+        {
+            SemaphoreSlim.Wait();
+            //Mutex.WaitOne();
+            // Mutex acquired
+        }
+        catch (AbandonedMutexException)
+        {
+            Console.WriteLine("Warning: Mutex was abandoned by another thread.");
+        }
     }
 
-    public static class ThreadBlocking
+    public static void Release()
     {
-        private static readonly Mutex Mutex = new();
-        private static Thread _owningThread;
-
-        public static void Wait()
-        {
-            try
-            {
-                Mutex.WaitOne();
-                _owningThread = Thread.CurrentThread;
-                // Mutex acquired
-            }
-            catch (AbandonedMutexException)
-            {
-                Console.WriteLine("Warning: Mutex was abandoned by another thread.");
-            }
-        }
-
-        public static void Release()
-        {
-            if (_owningThread == Thread.CurrentThread)
-            {
-                _owningThread = null; // Reset ownership before releasing
-                Mutex.ReleaseMutex();
-            }
-            else
-            {
-                Console.WriteLine("Warning: Mutex was not owned by the current thread.");
-            }
-        }
+        SemaphoreSlim.Release();
+        // if (Mutex.WaitOne(0)) // Check if mutex is owned before releasing
+        // {
+        //     Mutex.ReleaseMutex();
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Warning: Mutex was not owned by the current thread.");
+        // }
     }
 }
