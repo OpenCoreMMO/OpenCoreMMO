ac1d3a0a61dbc572247083d2b82d598b708e52b5
caioavidal <caio.avidal@gmail.com>
2024-12-26 19:49:06 -0300

    Fix unit tests

--- tests/NeoServer.Game.Items.Tests/Inspection/InspectionTextBuilderTest.cs
+++ tests/NeoServer.Game.Items.Tests/Inspection/InspectionTextBuilderTest.cs
@@ -14,26 +14,15 @@ public class InspectionTextBuilderTest
 {
     [Theory]
     [InlineData("You see  item.")]
-    [InlineData("You see  item.\nIt can only be wielded properly by knights and paladins.", 1, 2)]
-    [InlineData("You see  item.\nIt can only be wielded properly by knights.", 1)]
-    [InlineData("You see  item.\nIt can only be wielded properly by knights, paladins and sorcerers.", 1, 2, 3)]
-    [InlineData("You see  item.\nIt can only be wielded properly by knights, paladins, sorcerers and druids.", 1, 2, 3,
-        4)]
-    public void Add_HasVocations_ReturnText(string expected, params int[] vocations)
+    [InlineData("You see  item.\nIt can only be wielded properly by knights and paladins.", "knight", "paladin")]
+    [InlineData("You see  item.\nIt can only be wielded properly by knights.", "knight")]
+    [InlineData("You see  item.\nIt can only be wielded properly by knights, paladins and sorcerers.", "knight", "paladin", "sorcerer")]
+    [InlineData("You see  item.\nIt can only be wielded properly by knights, paladins, sorcerers and druids.", "knight", "paladin", "sorcerer", "druid")]
+    public void Add_HasVocations_ReturnText(string expected, params string[] vocations)
     {
-        var vocationStore = new VocationStore();
-        vocationStore.Add(1, new Vocation { Name = "Knight" });
-        vocationStore.Add(2, new Vocation { Name = "Paladin" });
-        vocationStore.Add(3, new Vocation { Name = "Sorcerer" });
-        vocationStore.Add(4, new Vocation { Name = "Druid" });
-
-        var input = vocations.Select(x => (byte)x).ToArray();
-
         var item = ItemTestData.CreateDefenseEquipmentItem(1);
-        item.Metadata.Attributes.SetAttribute(ItemAttribute.Vocation, input);
-
-        var player = PlayerTestDataBuilder.Build(hp: 200);
-
+        item.Metadata.Attributes.SetAttribute(ItemAttribute.VocationNames, vocations);
+        
         //act
         var actual = InspectionTextBuilder.Build(item);
 
