c59e82e1ad9846a94588755ae531084db72d6059
Felipe Muniz <felipemuniznet@gmail.com>
2024-12-29 21:09:36 -0300

    feat: try remove duplicated folder Tools/tools

---
+++ data/LuaJit/scripts/actions/tools/shovel.lua
@@ -0,0 +1,8 @@
+﻿local shovel = Action()
+
+function shovel.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	return onUseShovel(player, item, fromPosition, target, toPosition, isHotkey)
+end
+
+shovel:id(2554, 5710)
+shovel:register()
