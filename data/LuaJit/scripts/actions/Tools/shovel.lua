43638a8a77f196b98dd89d6906f2e326c2ac8759
Felipe Muniz <felipemuniznet@gmail.com>
2024-12-29 21:08:01 -0300

    feat: try remove duplicated folder Tools/tools

--- data/LuaJit/scripts/actions/Tools/shovel.lua
+++
@@ -1,8 +0,0 @@
-﻿local shovel = Action()
-
-function shovel.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	return onUseShovel(player, item, fromPosition, target, toPosition, isHotkey)
-end
-
-shovel:id(2554, 5710)
-shovel:register()
--- data/LuaJit/scripts/actions/tools/kitchen_knife.lua
+++
@@ -1,8 +0,0 @@
-﻿local knife = Action()
-
-function knife.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	return onUseKitchenKnife(player, item, fromPosition, target, toPosition, isHotkey)
-end
-
-knife:id(2566)
-knife:register()
