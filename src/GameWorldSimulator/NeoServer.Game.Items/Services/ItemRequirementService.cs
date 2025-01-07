3876aa34411efcfe3fcf4de707ac034475d342f6
caioavidal <caio.avidal@gmail.com>
2025-01-07 18:36:24 -0300

    Remove PremiumTime column and create ItemRequirementService.cs

---
+++ src/GameWorldSimulator/NeoServer.Game.Items/Services/ItemRequirementService.cs
@@ -0,0 +1,59 @@
+using NeoServer.Game.Common;
+using NeoServer.Game.Common.Contracts.Creatures;
+using NeoServer.Game.Common.Contracts.Items;
+using NeoServer.Game.Common.Contracts.Services;
+using NeoServer.Game.Common.Creatures;
+using NeoServer.Game.Common.Creatures.Players;
+using NeoServer.Game.Common.Results;
+
+namespace NeoServer.Game.Items.Services;
+
+public class ItemRequirementService : IItemRequirementService
+{
+    public Result PlayerCanUseItem(IPlayer player, IItem item, Requirement requirement)
+    {
+        if (player.Level < requirement.MinLevel) return Result.Fail(InvalidOperation.NotEnoughLevel);
+
+        var hasRequiredVocation = PlayerHasRequiredVocation(player, requirement);
+        if (!hasRequiredVocation) return Result.Fail(InvalidOperation.VocationCannotUseSpell);
+
+        if ((player.Skills[SkillType.Magic]?.Level ?? 0) < requirement.MinMagicLevel)
+            return Result.Fail(InvalidOperation.NotEnoughMagicLevel);
+
+        if (player.PremiumTime <= 0 && requirement.RequirePremiumTime)
+            return Result.Fail(InvalidOperation.PremiumTimeIsRequired);
+
+        return Result.Success;
+    }
+
+    public Result PlayerCanEquipItem(IPlayer player, IItem item, Requirement requirement)
+    {
+        if (player.Level < requirement.MinLevel) return Result.Fail(InvalidOperation.NotEnoughLevel);
+
+        var hasRequiredVocation = PlayerHasRequiredVocation(player, requirement);
+        if (!hasRequiredVocation) return Result.Fail(InvalidOperation.VocationCannotUseSpell);
+
+        if ((player.Skills[SkillType.Magic]?.Level ?? 0) < requirement.MinMagicLevel)
+            return Result.Fail(InvalidOperation.NotEnoughMagicLevel);
+
+        if (player.PremiumTime <= 0 && requirement.RequirePremiumTime)
+            return Result.Fail(InvalidOperation.PremiumTimeIsRequired);
+
+        if (requirement.Slot is not Slot.None && item.Metadata.BodyPosition != requirement.Slot)
+            return Result.Fail(InvalidOperation.CannotDress);
+
+        return Result.Success;
+    }
+
+    private static bool PlayerHasRequiredVocation(IPlayer player, Requirement requirement)
+    {
+        if (requirement.RequiredVocations is null) return true;
+
+        foreach (var requiredVocation in requirement.RequiredVocations)
+        {
+            if (player.VocationType == requiredVocation) return true;
+        }
+
+        return false;
+    }
+}
