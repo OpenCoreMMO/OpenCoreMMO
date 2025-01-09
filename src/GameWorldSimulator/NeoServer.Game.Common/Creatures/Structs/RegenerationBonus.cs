31faf8e829d8900554f83f425f98b56ff7a00fd5
caioavidal <caio.avidal@gmail.com>
2025-01-09 08:14:53 -0300

    Add comment explaining the RegenerationBonus Id generation

--- src/GameWorldSimulator/NeoServer.Game.Common/Creatures/Structs/RegenerationBonus.cs
+++ src/GameWorldSimulator/NeoServer.Game.Common/Creatures/Structs/RegenerationBonus.cs
@@ -2,7 +2,7 @@ namespace NeoServer.Game.Common.Creatures.Structs;
 
 public readonly record struct RegenerationBonus
 {
-    public ulong Id => ((ulong)Ticks << 16) | Gain;
+    public ulong Id => ((ulong)Ticks << 16) | Gain; // Shift the uint part to the left by 16 bits and OR with ushort part
     public required int Ticks { get; init; }
     public required ushort Gain { get; init; }
     public required RegenerationType Type { get; init; }
