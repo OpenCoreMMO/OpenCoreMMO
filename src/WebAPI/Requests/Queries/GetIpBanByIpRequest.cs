1287779a44dc23779264a79d3717d19829728057
MarcusViniciusS <lowpoc.developer@gmail.com>
2025-01-15 12:35:33 -0300

    feat: create ipbans module on webapi.

---
+++ src/WebAPI/Requests/Queries/GetIpBanByIpRequest.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using NeoServer.Web.API.Response.IpBans;
+
+namespace NeoServer.Web.API.Requests.Queries;
+
+public record GetIpBanByIpRequest(string ip) : IRequest<IpBanResponseViewModel>;
