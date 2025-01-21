98719ba6e4a9cfc8842fd30f36536279534c0c6b
Felipe Muniz <felipemuniznet@gmail.com>
2025-01-21 15:18:09 -0300

    fix: adjustments on tests

---
+++ tests/NeoServer.Game.Tests/Server/ScriptManagerTestBuilder.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using NeoServer.Game.Common.Contracts.Items;
+using NeoServer.Scripts.LuaJIT;
+using NeoServer.Scripts.LuaJIT.Interfaces;
+using NeoServer.Server.Common.Contracts.Scripts;
+using NeoServer.Server.Common.Contracts.Scripts.Services;
+using Serilog;
+
+namespace NeoServer.Game.Tests.Server;
+
+public static class ScriptManagerTestBuilder
+{
+    public static IScriptManager Build(params IItem[] items)
+    {
+        return new LuaScriptManager(
+            new Mock<ILuaStartup>().Object,
+            new Mock<IGlobalEvents>().Object,
+            new Mock<ILogger>().Object,
+            new Mock<IActionScriptService>().Object,
+            new Mock<ICreatureEventsScriptService>().Object,
+            new Mock<IGlobalEventsScriptService>().Object,
+            new Mock<IMoveEventsScriptService>().Object,
+            new Mock<ITalkActionScriptService>().Object
+        );
+    }
+}
