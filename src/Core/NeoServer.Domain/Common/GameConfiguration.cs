c6f911a1c29aaa68eae81d2138cf418cb327883c
caioavidal <caio.avidal@gmail.com>
2025-06-11 17:51:50 -0300

    Fix unit test

--- src/Core/NeoServer.Domain/Common/GameConfiguration.cs
+++ src/Core/NeoServer.Domain/Common/GameConfiguration.cs
@@ -15,7 +15,7 @@ public record GameConfiguration(
     ReportConfiguration Report = null
 );
 
-public record CombatConfiguration(bool InfiniteAmmo, bool InfiniteThrowingWeapon, decimal AttackSpeedMultiplier);
+public record CombatConfiguration(bool InfiniteAmmo = false, bool InfiniteThrowingWeapon = false, decimal AttackSpeedMultiplier = 1);
 
 public record DeathConfiguration
 {
