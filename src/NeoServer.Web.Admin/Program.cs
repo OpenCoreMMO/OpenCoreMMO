c8f6e85708dfec645c8ee84262d82637b9a5c333
Felipe Muniz <felipemuniznet@gmail.com>
2025-07-07 16:15:11 -0300

    feat: update compose files, create dockerfile and ocmpose to new webadmin project, refact in webapi and webadmin to shared common classes.

--- src/NeoServer.Web.Admin/Program.cs
+++ src/NeoServer.Web.Admin/Program.cs
@@ -4,9 +4,19 @@ using NeoServer.Web.Admin;
 using NeoServer.Web.Admin.Components;
 using NeoServer.Web.API.IoC.Modules;
 using NeoServer.Web.API.Requests.Validators;
+using System;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
+var environment = builder.Environment;
+
+// Configure appsettings
+builder.Configuration
+    .SetBasePath(environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", true)
+    .AddEnvironmentVariables();
 
 // Add MudBlazor services
 builder.Services.AddMudServices();
@@ -16,7 +26,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddServicesApi();
-builder.Services.AddAutoMapperProfiles();
+builder.Services.AddAutoMapperProfiles(typeof(Program).Assembly);
 
 builder.Services.AddLogger(configuration);
 builder.Services.AddDatabases(configuration);
@@ -24,10 +34,19 @@ builder.Services.AddRepositories();
 
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssembly(typeof(NeoServer.Web.API.Program).Assembly);
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
 
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardLimit = 2;
+    options.KnownProxies.Add(IPAddress.Parse("127.0.0.1"));
+    options.KnownProxies.Add(IPAddress.Parse("0.0.0.0"));
+    options.ForwardedForHeaderName = "X-Forwarded-For-My-Custom-Header-Name";
+});
+
+builder.Services.AddCors();
 
 builder.Services.AddScoped<ProgressBarState>();
 
@@ -43,6 +62,10 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
 
 app.UseAntiforgery();
 
