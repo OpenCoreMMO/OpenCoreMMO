f97dbebc5d025d48263d44566bb5d4bba72e4569
Caio Vidal <caio.avidal@gmail.com>
2025-07-08 16:45:38 -0300

    Update src/Extensions/NeoServer.Scripts.LuaJIT/Models/Combat/LuaCombat.cs
    Co-authored-by: codefactor-io[bot] <47775046+codefactor-io[bot]@users.noreply.github.com>

--- src/Extensions/NeoServer.Scripts.LuaJIT/Models/Combat/LuaCombat.cs
+++ src/Extensions/NeoServer.Scripts.LuaJIT/Models/Combat/LuaCombat.cs
@@ -80,7 +80,7 @@ public class LuaCombat : Script
             MinDamage = (ushort)damageValues.Min,
             MaxDamage = (ushort)damageValues.Max,
             Range = 7,
-            Area = Areas.Any() ? Areas[direction] : null,
+            Area = Areas.Count != 0 ? Areas[direction] : null,
         };
     }
 }
