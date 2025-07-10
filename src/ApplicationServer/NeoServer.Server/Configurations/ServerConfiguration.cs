cb61e59ed6357bf9e1757c77a0399c54d0447583
Felipe Muniz <felipemuniznet@gmail.com>
2025-07-09 22:50:25 -0300

    fix: add changes on GrayLogConfiguration

--- src/ApplicationServer/NeoServer.Server/Configurations/ServerConfiguration.cs
+++ src/ApplicationServer/NeoServer.Server/Configurations/ServerConfiguration.cs
@@ -34,6 +34,7 @@ public record DatabaseConfiguration(Dictionary<DatabaseType, string> Connections
 
 [SuppressMessage("ReSharper", "InconsistentNaming")]
 public record GrayLogConfiguration(
+    bool enable,
     string HostnameOrAddress,
     int Port,
     string HostnameOverride,
