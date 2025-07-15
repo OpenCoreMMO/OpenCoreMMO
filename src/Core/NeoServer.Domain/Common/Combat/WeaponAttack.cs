55a5fb7f90ccd18b4819125c7e7c8f53d2601559
Felipe Muniz <felipemuniznet@gmail.com>
2025-07-15 18:40:29 -0300

    fix: adjustments to tests pass

--- src/Core/NeoServer.Domain/Common/Combat/WeaponAttack.cs
+++ src/Core/NeoServer.Domain/Common/Combat/WeaponAttack.cs
@@ -9,8 +9,8 @@ public readonly struct WeaponAttack(
 {
     public ushort AttackPower =>
         itemAttributes != null && itemAttributes.TryGetValue(ItemAttribute.Attack, out var attack)
-            ? (ushort)attack
-            : metadata?.AttackPower ?? 0;
+        ? Convert.ToUInt16(attack)
+        : metadata != null ? metadata.AttackPower : (ushort)0;
 
     public byte AttackPowerPercentage => TotalAttackPower is 0 ? (byte)0 : (byte)(AttackPower * 100 / TotalAttackPower);
 
