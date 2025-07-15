9a1ec5187b31eeb3a8879f3528057952c9f3b656
Felipe Muniz <felipemuniznet@gmail.com>
2025-07-14 23:45:35 -0300

    Update tests/NeoServer.Domain.Tests/Items/Items/MagicWeaponTests.cs
    Co-authored-by: codefactor-io[bot] <47775046+codefactor-io[bot]@users.noreply.github.com>

--- tests/NeoServer.Domain.Tests/Items/Items/MagicWeaponTests.cs
+++ tests/NeoServer.Domain.Tests/Items/Items/MagicWeaponTests.cs
@@ -12,7 +12,7 @@ public class MagicWeaponTests
     [Fact]
     public void InspectionText_NoAttributeFound_ReturnsText()
     {
-        var sut = ItemTestData.CreateMagicWeapon(1, attributes: new (ItemTypeAttribute, IConvertible)[]
+        var sut = ItemTestData.CreateMagicWeapon(1, attributes: Array.Empty<(ItemTypeAttribute, IConvertible)>());
         {
         });
 
