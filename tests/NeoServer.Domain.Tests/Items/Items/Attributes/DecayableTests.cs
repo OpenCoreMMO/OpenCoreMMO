39091b32d078e55dc1c4c333f69be9c91d8e2c68
Felipe Muniz <felipemuniznet@gmail.com>
2025-07-14 23:42:43 -0300

    Update tests/NeoServer.Domain.Tests/Items/Items/Attributes/DecayableTests.cs
    Co-authored-by: codefactor-io[bot] <47775046+codefactor-io[bot]@users.noreply.github.com>

--- tests/NeoServer.Domain.Tests/Items/Items/Attributes/DecayableTests.cs
+++ tests/NeoServer.Domain.Tests/Items/Items/Attributes/DecayableTests.cs
@@ -30,7 +30,7 @@ public class DecayableTests : IAsyncLifetime
     {
         //arrange
 
-        var item = ItemTestData.CreateDefenseEquipmentItem(2, attributes: new (ItemTypeAttribute, IConvertible)[]
+        var item = ItemTestData.CreateDefenseEquipmentItem(2, attributes: Array.Empty<(ItemTypeAttribute, IConvertible)>());
         {
         });
         var decayableItem = ItemTestData.CreateDefenseEquipmentItem(1, attributes: new (ItemTypeAttribute, IConvertible)[]
