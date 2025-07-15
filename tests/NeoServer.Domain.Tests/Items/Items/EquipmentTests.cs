1b385044c0af41964933e0ad5c5c4d7733e8603d
Felipe Muniz <felipemuniznet@gmail.com>
2025-07-14 23:43:50 -0300

    Update tests/NeoServer.Domain.Tests/Items/Items/EquipmentTests.cs
    Co-authored-by: codefactor-io[bot] <47775046+codefactor-io[bot]@users.noreply.github.com>

--- tests/NeoServer.Domain.Tests/Items/Items/EquipmentTests.cs
+++ tests/NeoServer.Domain.Tests/Items/Items/EquipmentTests.cs
@@ -581,7 +581,7 @@ public class EquipmentTests : IAsyncLifetime
         var player = PlayerTestDataBuilder.Build();
 
         var decaysTo = ItemTestData.CreateDefenseEquipmentItem(3, "ring",
-            attributes: new (ItemTypeAttribute, IConvertible)[]
+            attributes: Array.Empty<(ItemTypeAttribute, IConvertible)>());
             {
             });
         var itemTypeStore = ItemTestData.GetItemTypeStore(decaysTo.Metadata);
