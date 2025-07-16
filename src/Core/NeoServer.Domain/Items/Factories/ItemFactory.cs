48140109f899b8ff1b250cdbfdb20e129177bd18
Felipe Muniz <felipemuniznet@gmail.com>
2025-07-16 19:13:11 -0300

    fix: cumulative items amoun on relogin

--- src/Core/NeoServer.Domain/Items/Factories/ItemFactory.cs
+++ src/Core/NeoServer.Domain/Items/Factories/ItemFactory.cs
@@ -130,7 +130,6 @@ public class ItemFactory : IItemFactory
         return createdItem;
     }
 
-
     public IEnumerable<Coin> CreateCoins(ulong amount)
     {
         var coinsToAdd = CoinCalculator.Calculate(CoinTypeStore.Map, amount);
