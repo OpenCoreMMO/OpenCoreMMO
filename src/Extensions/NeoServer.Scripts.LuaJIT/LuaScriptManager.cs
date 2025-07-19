70526c076dab4e1763ba50944eece73cfbe904db
Felipe Muniz <felipemuniznet@gmail.com>
2025-07-19 11:41:59 -0300

    feat: auto reload scripts implementation

--- src/Extensions/NeoServer.Scripts.LuaJIT/LuaScriptManager.cs
+++ src/Extensions/NeoServer.Scripts.LuaJIT/LuaScriptManager.cs
@@ -1,34 +1,40 @@
-﻿using NeoServer.Scripts.LuaJIT.Interfaces;
+﻿using NeoServer.Domain.Common;
+using NeoServer.Scripts.LuaJIT.Interfaces;
 using NeoServer.Server.Common.Contracts.Scripts;
 using NeoServer.Server.Common.Contracts.Scripts.Services;
+using NeoServer.Server.Configurations;
 using Serilog;
 
 namespace NeoServer.Scripts.LuaJIT;
 
-public class LuaScriptManager : IScriptManager
+public class LuaScriptManager(
+    ILuaStartup luaStartup,
+    IGlobalEvents globalEvents,
+    ILogger logger,
+    IActionScriptService actionsScriptService,
+    ICreatureEventsScriptService creatureEventsScriptService,
+    IGlobalEventsScriptService globalEventsScriptService,
+    IMoveEventsScriptService moveEventsScriptService,
+    ITalkActionScriptService talkActionsScriptService,
+    IReloadManager reloadManager,
+    ServerConfiguration serverConfiguration) : IScriptManager
 {
-    #region Constructors
+    #region Private Members
 
-    public LuaScriptManager(
-        ILuaStartup luaStartup,
-        IGlobalEvents globalEvents,
-        ILogger logger,
-        IActionScriptService actionsScriptService,
-        ICreatureEventsScriptService creatureEventsScriptService,
-        IGlobalEventsScriptService globalEventsScriptService,
-        IMoveEventsScriptService moveEventsScriptService,
-        ITalkActionScriptService talkActionsScriptService)
-    {
-        _luaStartup = luaStartup;
-        _globalEvents = globalEvents;
-        _logger = logger;
+    private static FileSystemWatcher _scriptsWatcher;
+    private static Timer _debounceTimer;
+    private static readonly object _debounceLock = new();
+    private static bool _pendingReload;
 
-        Actions = actionsScriptService;
-        CreatureEvents = creatureEventsScriptService;
-        GlobalEvents = globalEventsScriptService;
-        MoveEvents = moveEventsScriptService;
-        TalkActions = talkActionsScriptService;
-    }
+    #endregion
+
+    #region Properties
+
+    public IActionScriptService Actions { get; } = actionsScriptService;
+    public ICreatureEventsScriptService CreatureEvents { get; } = creatureEventsScriptService;
+    public IGlobalEventsScriptService GlobalEvents { get; } = globalEventsScriptService;
+    public IMoveEventsScriptService MoveEvents { get; } = moveEventsScriptService;
+    public ITalkActionScriptService TalkActions { get; } = talkActionsScriptService;
 
     #endregion
 
@@ -36,53 +42,71 @@ public class LuaScriptManager : IScriptManager
 
     public void Initialize()
     {
-        _luaStartup.Start();
-        _globalEvents.Startup();
+        luaStartup.Start();
+        globalEvents.Startup();
+
+        if (serverConfiguration.AutoReloadScripts)
+        {
+            SetupAutoReloadLuaScripts();
+            logger.Information("Auto Reload Lua Scripts is Enabled");
+        }
+        else
+        {
+            logger.Information("Auto Reload Lua Scripts is Disabled");
+        }
     }
 
     #endregion
 
-    #region Dependency Injections
+    public void Reload(ReloadType reloadType)
+    {
+        reloadManager.Reload(reloadType);
+        logger.Information("Reloaded {reloadType} Lua Scripts", reloadType);
+    }
 
-    /// <summary>
-    ///     A reference to the <see cref="ILuaStartup" /> instance in use.
-    /// </summary>
-    private readonly ILuaStartup _luaStartup;
+    #region Private Methods
 
-    /// <summary>
-    ///     A reference to the <see cref="ILuaStartup" /> instance in use.
-    /// </summary>
-    private readonly IGlobalEvents _globalEvents;
+    private void SetupAutoReloadLuaScripts()
+    {
+        _scriptsWatcher = new FileSystemWatcher(serverConfiguration.Data, "*.lua");
+        _scriptsWatcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.Size;
+        _scriptsWatcher.IncludeSubdirectories = true;
 
-    /// <summary>
-    ///     A reference to the <see cref="ILogger" /> instance in use.
-    /// </summary>
-    private readonly ILogger _logger;
+        void OnFileChanged(object sender, FileSystemEventArgs e)
+        {
+            lock (_debounceLock)
+            {
+                _pendingReload = true;
+                //Reset timer: only reloads if there are no events for 1 second
+                _debounceTimer?.Change(Timeout.Infinite, Timeout.Infinite);
+                if (_debounceTimer == null)
+                {
+                    _debounceTimer = new Timer(_ =>
+                    {
+                        lock (_debounceLock)
+                        {
+                            if (_pendingReload)
+                            {
+                                Reload(ReloadType.All);
+                                _pendingReload = false;
+                            }
+                        }
+                    }, null, 1000, Timeout.Infinite);
+                }
+                else
+                {
+                    _debounceTimer.Change(1000, Timeout.Infinite);
+                }
+            }
+        }
 
-    /// <summary>
-    ///     A reference to the <see cref="IActionScriptService" /> instance in use.
-    /// </summary>
-    public IActionScriptService Actions { get; }
+        _scriptsWatcher.Changed += OnFileChanged;
+        _scriptsWatcher.Created += OnFileChanged;
+        _scriptsWatcher.Deleted += OnFileChanged;
+        _scriptsWatcher.Renamed += OnFileChanged;
 
-    /// <summary>
-    ///     A reference to the <see cref="ICreatureEventsScriptService" /> instance in use.
-    /// </summary>
-    public ICreatureEventsScriptService CreatureEvents { get; }
-
-    /// <summary>
-    ///     A reference to the <see cref="IGlobalEventsScriptService" /> instance in use.
-    /// </summary>
-    public IGlobalEventsScriptService GlobalEvents { get; }
-
-    /// <summary>
-    ///     A reference to the <see cref="IMoveEventsScriptService" /> instance in use.
-    /// </summary>
-    public IMoveEventsScriptService MoveEvents { get; }
-
-    /// <summary>
-    ///     A reference to the <see cref="ITalkActionScriptService" /> instance in use.
-    /// </summary>
-    public ITalkActionScriptService TalkActions { get; }
+        _scriptsWatcher.EnableRaisingEvents = true;
+    }
 
     #endregion
 }
