72b352d4f5fca241903a6a9c6fe63c078626809c
Felipe Muniz <felipemuniznet@gmail.com>
2025-07-24 01:58:33 -0300

    fix: codefactor adjustments

---
+++ src/Extensions/NeoServer.Scripts.LuaJIT/Events/Callbacks/Player/PlayerThinkUpdateEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using NeoServer.Domain.Common;
+using NeoServer.Domain.Creatures.Events.Player;
+using NeoServer.Scripts.LuaJIT.Enums;
+using NeoServer.Scripts.LuaJIT.Interfaces;
+
+namespace NeoServer.Scripts.LuaJIT.Events.Callbacks.Player;
+
+public class PlayerThinkUpdateEventHandler(IEventsCallbacks eventsCallbacks)
+    : IApplicationEventHandler<PlayerThinkEvent>
+{
+    public void Handle(PlayerThinkEvent @event)
+    {
+        eventsCallbacks.ExecuteCallback(
+                EventCallbackType.PlayerOnThink,
+                callback =>
+                    callback.PlayerOnThink(@event.Player, @event.Interval));
+    }
+}
