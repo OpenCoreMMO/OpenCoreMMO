bc9568edca14e0ec4420bd88d6dc616b856f2028
Felipe Muniz <felipemuniznet@gmail.com>
2025-08-07 14:40:46 -0300

    fix: login packets (#789)
    ### Description
    minimal adjustments accountLogin and playerLogin packets and exception
    fix on Rsa.Decrypt
    
    ### Types of Changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Documentation fix (typos, incorrect content, missing content,
    etc.)
    - [ ] Code refactoring
    - [ ] Merge Down

--- src/NetworkingServer/NeoServer.Networking.Packets/Incoming/PlayerLoginPacket.cs
+++ src/NetworkingServer/NeoServer.Networking.Packets/Incoming/PlayerLoginPacket.cs
@@ -10,24 +10,19 @@ public class PlayerLogInPacket : IncomingPacket
 {
     public PlayerLogInPacket(IReadOnlyNetworkMessage message)
     {
-        var packetLength = message.GetUInt16();
-        var tcpPayload = packetLength + 2;
-        message.SkipBytes(5);
-
+        message.SkipBytes(7);
         OperatingSystem = (OperatingSystem)message.GetUInt16();
         Version = message.GetUInt16();
 
         //message.SkipBytes(9);
 
         //// todo: version validation
-
-        var encryptedDataLength = tcpPayload - message.BytesRead;
-        var encryptedData = message.GetBytes(encryptedDataLength);
+        var encryptedData = message.GetBytes(Rsa.LENGTH);
 
         var decryptedData = Rsa.Decrypt(encryptedData.ToArray());
         if (decryptedData is null || decryptedData.Length == 0) return;
 
-        var data = new ReadOnlyNetworkMessage(decryptedData, encryptedDataLength);
+        var data = new ReadOnlyNetworkMessage(decryptedData, encryptedData.Length);
 
         LoadXtea(data);
 
