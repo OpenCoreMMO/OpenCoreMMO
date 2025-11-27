0a3e57cfb9f57e236fd2d909032ba924c74ba538
Caio Vidal <caio.avidal@gmail.com>
2025-11-27 17:29:30 -0300

    Update src/Core/NeoServer.Domain/Common/Contracts/Creatures/IMonster.cs
    Co-authored-by: codefactor-io[bot] <47775046+codefactor-io[bot]@users.noreply.github.com>

--- src/Core/NeoServer.Domain/Common/Contracts/Creatures/IMonster.cs
+++ src/Core/NeoServer.Domain/Common/Contracts/Creatures/IMonster.cs
@@ -7,7 +7,6 @@ using NeoServer.Domain.Creatures.Monster;
 using NeoServer.Domain.Creatures.Monster.Combat;
 
 
-
 public delegate void MonsterChangeState(IMonster monster, MonsterState fromState, MonsterState toState);
 
 public interface IMonster : IWalkableMonster, ICombatActor
