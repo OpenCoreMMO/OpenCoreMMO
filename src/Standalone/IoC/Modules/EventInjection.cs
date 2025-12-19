93ebb9823223a628efbdc11354f4e626c773c77e
caioavidal <caio.avidal@gmail.com>
2025-12-19 12:48:19 -0300

    refactor: replace static extension methods in `EventInjection` with instance methods for improved modularity
    - Transitioned static extension methods in `EventInjection` to instance methods for cleaner structure and maintainability.
    - Updated registration methods (`RegisterServerEvents`, `RegisterGameEvents`, `RegisterNetworkEvents`, `RegisterEventSubscribers`) to align with the new instance-based design.

--- src/Standalone/IoC/Modules/EventInjection.cs
+++ src/Standalone/IoC/Modules/EventInjection.cs
@@ -13,41 +13,44 @@ namespace NeoServer.Server.Standalone.IoC.Modules;
 
 public static class EventInjection
 {
-    public static IServiceCollection AddEvents(this IServiceCollection builder)
+    extension(IServiceCollection builder)
     {
-        builder.RegisterServerEvents();
-        builder.RegisterGameEvents();
-        builder.RegisterNetworkEvents();
-        builder.RegisterEventSubscribers();
-        builder.AddSingleton<EventSubscriber>();
-        builder.AddSingleton<FactoryEventSubscriber>();
+        public IServiceCollection AddEvents()
+        {
+            builder.RegisterServerEvents();
+            builder.RegisterGameEvents();
+            builder.RegisterNetworkEvents();
+            builder.RegisterEventSubscribers();
+            builder.AddSingleton<EventSubscriber>();
+            builder.AddSingleton<FactoryEventSubscriber>();
 
-        return builder;
-    }
+            return builder;
+        }
 
-    private static void RegisterServerEvents(this IServiceCollection builder)
-    {
-        var assembly = Assembly.GetAssembly(typeof(CreatureChangedVisibilityEventHandler));
-        builder.RegisterAssemblyTypes(assembly);
-    }
+        private void RegisterServerEvents()
+        {
+            var assembly = Assembly.GetAssembly(typeof(CreatureChangedVisibilityEventHandler));
+            builder.RegisterAssemblyTypes(assembly);
+        }
 
-    private static void RegisterNetworkEvents(this IServiceCollection builder)
-    {
-        builder.RegisterAssembliesByInterface(typeof(INetworkEventHandler<>));
-    }
+        private void RegisterNetworkEvents()
+        {
+            builder.RegisterAssembliesByInterface(typeof(INetworkEventHandler<>));
+        }
 
-    private static void RegisterGameEvents(this IServiceCollection builder)
-    {
-        builder.RegisterAssembliesByInterface(typeof(IGameEventHandler));
-    }
+        private void RegisterGameEvents()
+        {
+            builder.RegisterAssembliesByInterface(typeof(IGameEventHandler));
+        }
 
-    private static void RegisterEventSubscribers(this IServiceCollection builder)
-    {
-        var types = Container.AssemblyCache;
+        private void RegisterEventSubscribers()
+        {
+            var types = Container.AssemblyCache;
 
-        builder
-            .RegisterAssemblyTypes<ICreatureEventSubscriber>(types)
-            .RegisterAssemblyTypes<IItemEventSubscriber>(types)
-            .RegisterAssemblyTypes<IChatChannelEventSubscriber>(types);
+            builder
+                .RegisterAssemblyTypes<ICreatureEventSubscriber>(types)
+                .RegisterAssemblyTypes<IItemEventSubscriber>(types)
+                .RegisterAssemblyTypes<IChatChannelEventSubscriber>(types);
+        }
     }
 }
