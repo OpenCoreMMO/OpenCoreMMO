01c4236713a27f91acd904bb1f4638f42a1107db
caioavidal <caio.avidal@gmail.com>
2026-05-04 08:57:01 -0300

    test: add unit tests for PlayerProtectionZoneBlock functionality

---
+++ tests/NeoServer.Domain.Tests/Creature/Players/PlayerProtectionZoneBlockTests.cs
@@ -0,0 +1,42 @@
+using NeoServer.Domain.Creatures.Conditions.Enums;
+using NeoServer.Domain.Creatures.Conditions.Implementations;
+using NeoServer.Domain.Tests.Helpers.Player;
+
+namespace NeoServer.Domain.Tests.Creature.Players;
+
+public class PlayerProtectionZoneBlockTests
+{
+    [Fact]
+    public void SetProtectionZoneBlock_adds_condition_when_not_blocked()
+    {
+        var player = PlayerTestDataBuilder.Build();
+
+        player.SetProtectionZoneBlock();
+
+        player.IsProtectionZoneBlocked.Should().BeTrue();
+    }
+
+    [Fact]
+    public void SetProtectionZoneBlock_does_nothing_when_pacified()
+    {
+        var player = PlayerTestDataBuilder.Build();
+        player.AddCondition(new PacifiedCondition());
+
+        player.SetProtectionZoneBlock();
+
+        player.IsProtectionZoneBlocked.Should().BeFalse();
+    }
+
+    [Fact]
+    public void SetProtectionZoneBlock_does_nothing_when_already_blocked()
+    {
+        var player = PlayerTestDataBuilder.Build();
+        player.SetProtectionZoneBlock();
+        player.IsProtectionZoneBlocked.Should().BeTrue();
+
+        player.SetProtectionZoneBlock();
+
+        var conditions = player.GetConditions();
+        conditions.Should().ContainSingle(c => c.Type == ConditionType.ProtectionZoneBlock);
+    }
+}
