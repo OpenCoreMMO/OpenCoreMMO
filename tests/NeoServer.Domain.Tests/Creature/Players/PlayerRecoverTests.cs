b516c2de7c85ff1ab2745f29f42d90323a29f757
caioavidal <caio.avidal@gmail.com>
2026-05-04 09:32:21 -0300

    test: add unit tests for AddRegenerationBonus and RemoveRegenerationBonus

--- tests/NeoServer.Domain.Tests/Creature/Players/PlayerRecoverTests.cs
+++ tests/NeoServer.Domain.Tests/Creature/Players/PlayerRecoverTests.cs
@@ -1,4 +1,5 @@
 using NeoServer.Domain.Common.Combat.Structs;
+using NeoServer.Domain.Common.Creatures.Structs;
 using NeoServer.Domain.Common.Item;
 using NeoServer.Domain.Tests.Helpers.Player;
 
@@ -47,5 +48,51 @@ public class PlayerRecoverTests
         player.Recover();
 
         player.HealthPoints.Should().Be(initialHp);
+    }
+
+    [Fact]
+    public void AddRegenerationBonus_increases_recovery()
+    {
+        var player = (NeoServer.Domain.Creatures.Player.Player)PlayerTestDataBuilder.Build();
+        var enemy = PlayerTestDataBuilder.Build();
+        player.Vocation.GainHpAmount = 0;
+        player.OnDamage(enemy, new CombatDamageList(new CombatDamage(80, DamageType.Melee)));
+        player.Feed(10);
+
+        player.AddRegenerationBonus(new RegenerationBonus
+        {
+            Ticks = 1000,
+            Gain = 50,
+            Type = RegenerationType.Health
+        });
+
+        var hpBeforeRecover = player.HealthPoints;
+        player.Recover();
+
+        player.HealthPoints.Should().BeGreaterThan(hpBeforeRecover);
+    }
+
+    [Fact]
+    public void RemoveRegenerationBonus_excludes_bonus_from_recovery()
+    {
+        var player = (NeoServer.Domain.Creatures.Player.Player)PlayerTestDataBuilder.Build();
+        var enemy = PlayerTestDataBuilder.Build();
+        player.Vocation.GainHpAmount = 0;
+        player.OnDamage(enemy, new CombatDamageList(new CombatDamage(20, DamageType.Melee)));
+        player.Feed(10);
+
+        var bonus = new RegenerationBonus
+        {
+            Ticks = 1000,
+            Gain = 50,
+            Type = RegenerationType.Health
+        };
+        player.AddRegenerationBonus(bonus);
+        player.RemoveRegenerationBonus(bonus);
+
+        var hpBeforeRecover = player.HealthPoints;
+        player.Recover();
+
+        player.HealthPoints.Should().Be(hpBeforeRecover);
     }
 }
